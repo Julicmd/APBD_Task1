3d0021b5716f8f649bb8235c191fe0f0a022aacc
Julius Babelis <s31850@pjwstk.edu.pl>
2026-03-10 17:04:40 +0100

    Sum the list

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Program{
+    public static void main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7};
+        int sum = 0;
+
+        foreach(int i in list)
+        {
+            sum += i;
+        }
+        Console.WriteLine("Sum of numbers: "+sum);
+
+    }
+}
+
