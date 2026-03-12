f54cd63082ec43ec9e6e0c879a2d88de51a94316
Julius Babelis <juli.b@172.23.85.182.wi-fi.pjwstk.edu.pl>
2026-03-12 17:02:06 +0100

    Checking list

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -6,6 +6,11 @@ public class Program{
 
         List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7};
         int sum = 0;
+
+        if (list == null || list.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+        }
         
         foreach(int i in list)
         {
@@ -13,6 +18,7 @@ public class Program{
             {
                 sum += i;
             }
+            
         }
         Console.WriteLine("Sum of even numbers: "+sum);
         
